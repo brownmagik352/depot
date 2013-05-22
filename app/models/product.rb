9ffe8d720262112ede17f87d4c98600a1e99c08c
brownmagik352 <asuman@Apurvs-MacBook-Pro.local>
2013-05-22 16:49:50 -0400

    agile ch. 7 playtime

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,19 +1,11 @@
 class Product < ActiveRecord::Base
-
-	validates :title, :description, :image_url,
-		presence: true #checks that these items are filled in
-
-	validates :price, 
-		numericality: {greater_than_or_equal_to: 0.01} #insures positive integer
-
-	validates :title, 
-		uniqueness: true #each product has unique title
-
-	validates :image_url, 
-		allow_blank: true, #avoid multiple error messages
-		format: { 
-			with: %r{\.(gif|jpg|png)\Z}i,
-			message: 'must be a URL for GIF, JPG, or PNG image.'
-		}
-
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  #validates :title, uniqueness: true
+  validates_uniqueness_of :title, :message => "must be original!!!"
+  validates_length_of :title, :minimum => 10, :message => "at least 10 characters" 
+  validates :image_url, allow_blank: true, format: {
+    with:    %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
 end
