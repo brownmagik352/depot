856403ead2188001a9f6eb478b3db3820341de24
brownmagik352 <asuman@Apurvs-MacBook-Pro.local>
2013-05-22 15:16:34 -0400

    agile 7.1: validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,19 @@
 class Product < ActiveRecord::Base
+
+	validates :title, :description, :image_url,
+		presence: true #checks that these items are filled in
+
+	validates :price, 
+		numericality: {greater_than_or_equal_to: 0.01} #insures positive integer
+
+	validates :title, 
+		uniqueness: true #each product has unique title
+
+	validates :image_url, 
+		allow_blank: true, #avoid multiple error messages
+		format: { 
+			with: %r{\.(gif|jpg|png)\Z}i,
+			message: 'must be a URL for GIF, JPG, or PNG image.'
+		}
+
 end
