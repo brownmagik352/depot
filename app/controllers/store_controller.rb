c42b363d3b8869501fd1937f3a77fd61a2d50c14
brownmagik352 <asuman@Apurvs-MacBook-Pro.local>
2013-05-29 10:29:55 -0400

    agile ch. 9

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,13 @@
 class StoreController < ApplicationController
   def index
 		@products = Product.order(:title) #list products in alphabetic order
+ 		@count = counter
+ 		@shown_message = "You have been here #{@count} times" if session[:count] > 5
   end
+
+  def counter
+  	session[:count] ||=1
+  	session[:count] +=1
+  end
+
 end
