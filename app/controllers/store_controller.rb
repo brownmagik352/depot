23226a7460f9d79d575639b0f9440dc4a97adbbb
brownmagik352 <asuman@Apurvs-MacBook-Pro.local>
2013-06-02 22:03:49 -0400

    agile ch.10 pt

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,15 +1,15 @@
+#---
+# Excerpted from "Agile Web Development with Rails",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
+#---
 class StoreController < ApplicationController
-	include CurrentCart
-	before_action :set_cart
+  include CurrentCart
+  before_action :set_cart
   def index
-		@products = Product.order(:title) #list products in alphabetic order
- 		@count = counter
- 		@shown_message = "You have been here #{@count} times" if session[:count] > 5
+    @products = Product.order(:title)
   end
-
-  def counter
-  	session[:count] ||=1
-  	session[:count] +=1
-  end
-
 end
